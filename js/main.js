3fd0750d25154cc404df8b72e1702956658bfcf2
goat95 <hjh9703@naver.com>
2022-05-01 00:57:09 +0900

    feat: Add Visual and Global Button

--- js/main.js
+++ js/main.js
@@ -19,7 +19,6 @@ const badgeEl = document.querySelector("header .badges");
 
 // _.throttle(함수, 시간)
 window.addEventListener("scroll", _.throttle(function () {
-  console.log(window.scrollY);
   if (window.scrollY > 500) {
     // 배지 숨기기
     // gsap.to(요소, 지속시간, 옵션);
